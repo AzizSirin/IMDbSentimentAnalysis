b2fb182018358fd7e3b56a24e5e67bde9e8696cb
Aziz Orhan Şirin <azizorhansirin@gmail.com>
2019-06-21 19:48:08 +0300

    Fixed few issues and comment added

--- data_preparing.py
+++ data_preparing.py
@@ -6,7 +6,7 @@ import time
 start_time = time.time()
 
 
-def create_database(name):
+def create_database(name):  # Creating database when needed..
 
     connection = sqlite3.connect('{}.db'.format(name), timeout=50)
     c = connection.cursor()
@@ -14,7 +14,7 @@ def create_database(name):
     return connection
 
 
-def create_connection(db_file):
+def create_connection(db_file):  # We need to create connection with the database where reviews and scores are stored
 
     try:
         conn = sqlite3.connect(db_file)
@@ -26,22 +26,25 @@ def create_connection(db_file):
     return None
 
 
-def fetch_from_database(score):
+def fetch_from_database(score):  # Function where you can fetch desired review set and append them into different database
+    count = 0
     score = int(score)
 
     conn = create_connection('2019-06.db')
     c = conn.cursor()
-    data = c.execute("""SELECT * FROM movie_reviews WHERE review_score=? LIMIT 3000""", (score,))
+    data = c.execute("""SELECT * FROM movie_reviews WHERE review_score=? LIMIT 3200""", (score,))
 
     score = str(score)
     connection_of_new_database = create_database(score)
     cursor = connection_of_new_database.cursor()
 
+    # Appending them into different databases, so it is easier to work on...
     for each in data:
-
+        count += 1
         cursor.execute("""INSERT INTO movie_reviews (review) VALUES (?)""", (each[0],))
         connection_of_new_database.commit()
-
+        if (count % 50) == 0:
+            print(" %s. review of score of %s has been appended" % (count , score))
     connection_of_new_database.close()
 
 
