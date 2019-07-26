48d7a8a3e2d149cbc51b54f9e5983bca4dd3b137
Aziz Orhan Şirin <azizorhansirin@gmail.com>
2019-07-26 19:54:44 +0300

    Fixed few issues

--- creating_dictionary.py
+++ creating_dictionary.py
@@ -9,12 +9,12 @@ start_time = time.time()
 def create_dictionary_database():
     connection = sqlite3.connect('dictionary.db', timeout=10000)
     c = connection.cursor()
-    c.execute("""CREATE TABLE IF NOT EXIST dictionary (word TEXT)""")
+    c.execute("""CREATE TABLE IF NOT EXISTS dictionary (word TEXT)""")
     connection.close()
 
 
 def fetch():
-
+    i = 0
     content = ''
 
     conn = create_connection("preprocessed_data")
@@ -23,6 +23,9 @@ def fetch():
 
     for review in data:
         content = content + ' ' + review[0]
+        i += 1
+        if i%250==0: print("%s reviews have been fetched." % (i))
+
     content = content.split()
     dictionary = set(content)
     conn.close()
@@ -31,10 +34,12 @@ def fetch():
 
     connection = create_connection("dictionary")
     cur = connection.cursor()
-
+    i = 0
     for word in dictionary:
-        cur.execute("""INSERT INTO dictionary (word) VALUES (?)""", (word))
-
+        cur.execute("""INSERT INTO dictionary (word) VALUES (?)""", (word,))
+        i += 1
+        if i%500==0: print("%s words are in dictionary." % (i))
+    connection.commit()
     connection.close()
 
 if __name__ == "__main__":
