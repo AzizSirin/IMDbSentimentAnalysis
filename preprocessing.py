98645632d9c374eee0bac15c9c579fbcee7fc22e
Aziz Orhan Şirin <azizorhansirin@gmail.com>
2020-04-15 21:10:17 +0100

    Altered algorithm to run faster.

--- preprocessing.py
+++ preprocessing.py
@@ -1,12 +1,10 @@
-import sqlite3
+import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-from nltk.stem import PorterStemmer
-from nltk.tokenize import word_tokenize
+from nltk.corpus import wordnet
 from sqlite3 import Error
 from multiprocessing import Pool
 import time
-from data_preparing import create_database
 from data_preparing import create_connection
 import re
 
@@ -14,56 +12,75 @@ REPLACE_NO_SPACE = re.compile("[.;:!\'<>£?,\"^+@#$½{}~´`%&/=_*(\-)\[\]]")
 LONG_WORDS = re.compile(r'\W*\b\w{40,999}\b')
 start_time = time.time()
 
+conn = create_connection('2020-04-11')
+c = conn.cursor()
+
 
 def filling_the_database(review_data):  # Appending the cleaned reviews to same database
     try:
-        conn = create_connection('preprocessed_data')
-        c = conn.cursor()
-        c.execute("""INSERT INTO movie_reviews (review, review_score) VALUES (?, ?)""", (review_data[0], review_data[1]))
+        c.executemany("""INSERT INTO preprocessed_reviews(review, review_score) VALUES (?, ?)""", review_data)
         conn.commit()
     except Error as e:
         print(e)
 
 
-def cleaner(review_data): # Cleaning the reviews
-    counter = 0
+def get_wordnet_pos(word):
+    """Map POS tag to first character lemmatize() accepts"""
+    tag = nltk.pos_tag([word])[0][1][0].upper()
+    tag_dict = {"J": wordnet.ADJ,
+                "N": wordnet.NOUN,
+                "V": wordnet.VERB,
+                "R": wordnet.ADV}
+
+    return tag_dict.get(tag, wordnet.NOUN)
+
+
+def review_cleaner(review):
     lemmatizer = WordNetLemmatizer()
-    stemmer = PorterStemmer()
-    for each in review_data:
-        cleaned_review = [REPLACE_NO_SPACE.sub(" ", each[0].lower())] # Turning capital letters to lower letter
-        cleaned_review = LONG_WORDS.sub("", cleaned_review[0]) # Removing punctuations
-        cleaned_review = ''.join([i for i in cleaned_review if not i.isdigit()]) # Removing numbers
-        cleaned_review = str.split(cleaned_review)
-        cleaned_review = ' '.join([word for word in cleaned_review if word not in stopwords.words('english')]) # Removing stopwords
-        cleaned_review = word_tokenize(cleaned_review)
-        stemmed_review = []
-        for word in cleaned_review:
-            word = lemmatizer.lemmatize(word) # Stemming
-            stemmed_review.append(stemmer.stem(word)) # Lemmatization
-        cleaned_review = ' '.join(stemmed_review)
-        review_and_score = [cleaned_review, each[1]]
-        filling_the_database(review_and_score)
-
-        counter += 1
-        if counter%25 == 0:
-            print("%s reviews of score %s have been cleaned." % (counter, each[1]))
-
-
-
-def main(path):
-
-    conn = create_connection(path)
-    c = conn.cursor()
-    data = c.execute("""SELECT * FROM movie_reviews LIMIT 3001""")
-
-    for each in data:
-        cleaner(data)
+    new_review_data = ()
+    bulk_data = []
+    for each in review:
+        # print('##BEFORE##', review[0])
+        review_temp = ''.join([i for i in each[0] if not i.isdigit()])
+        review_temp = REPLACE_NO_SPACE.sub(" ", review_temp.lower())
+        review_temp = nltk.word_tokenize(review_temp)
+        review_temp = (lemmatizer.lemmatize(word, get_wordnet_pos(word)) for word in review_temp)
+        review_temp = ' '.join([word for word in review_temp if word not in stopwords.words('english')])
+        new_review_data = (review_temp, each[1])
+        bulk_data.append(new_review_data)
+    filling_the_database(bulk_data)
+    # print('##AFTER##',review_temp)
 
 
 if __name__ == "__main__":
-    scores = [1,2,3,4,5,6,7,8,9,10]
-    c = create_database("preprocessed_data")
-    c.close()
-    p = Pool(10)
-    p.map(main, scores)
-    print("--- %s seconds ---" % (time.time() - start_time))
+    review_data = ()
+    bulk_data = []
+    amount_of_reviews = 0
+    previous_amount = 0
+    conn = create_connection('2020-04-11')
+    c = conn.cursor()
+    c.execute("""CREATE TABLE IF NOT EXISTS preprocessed_reviews(review TEXT, review_score INTEGER, ID PRIMARY KEY)""")
+    conn.commit()
+    total_number_of_reviews = c.execute("""SELECT COUNT(*) FROM movie_reviews""")
+    for each in total_number_of_reviews:
+        total_number_of_reviews = each[0]
+    counter = 0
+    while amount_of_reviews < 5000:
+        review_package = []
+        amount_of_reviews += 50
+        data = c.execute("""SELECT * FROM movie_reviews WHERE ID BETWEEN (?) AND (?)""", (previous_amount, amount_of_reviews-1))
+        conn.commit()
+        previous_amount = amount_of_reviews
+        for each in data:
+            review_data = (each[0], each[1])
+            review_package.append(review_data)
+            del review_data
+        bulk_data.append(review_package)
+        del review_package
+        print(amount_of_reviews)
+
+    # review_cleaner(bulk_data)
+    p = Pool(4)
+    p.map(review_cleaner, bulk_data)
+    p.close()
+    print('---- %s seconds ----' % (time.time() - start_time))
