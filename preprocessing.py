25607c08823bbe46003ade15375c4b986cc2cdf3
Aziz Orhan Şirin <azizorhansirin@gmail.com>
2020-04-19 19:56:16 +0100

    Used spaCy Lemmetizer instead of NLTK

--- preprocessing.py
+++ preprocessing.py
@@ -1,12 +1,11 @@
-import nltk
+
 from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import wordnet
 from sqlite3 import Error
 from multiprocessing import Pool
 import time
 from data_preparing import create_connection
 import re
+import spacy
 
 REPLACE_NO_SPACE = re.compile("[.;:!\'<>£?,\"^+@#$½{}~´`%&/=_*(\-)\[\]]")
 LONG_WORDS = re.compile(r'\W*\b\w{40,999}\b')
@@ -24,35 +23,22 @@ def filling_the_database(review_data):  # Appending the cleaned reviews to same 
         print(e)
 
 
-def get_wordnet_pos(word):
-    """Map POS tag to first character lemmatize() accepts"""
-    tag = nltk.pos_tag([word])[0][1][0].upper()
-    tag_dict = {"J": wordnet.ADJ,
-                "N": wordnet.NOUN,
-                "V": wordnet.VERB,
-                "R": wordnet.ADV}
-
-    return tag_dict.get(tag, wordnet.NOUN)
-
-
 def review_cleaner(review):
-    lemmatizer = WordNetLemmatizer()
-    new_review_data = ()
+    lemmatizer = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
     bulk_data = []
     for each in review:
-        # print('##BEFORE##', review[0])
         review_temp = ''.join([i for i in each[0] if not i.isdigit()])
         review_temp = REPLACE_NO_SPACE.sub(" ", review_temp.lower())
-        review_temp = nltk.word_tokenize(review_temp)
-        review_temp = (lemmatizer.lemmatize(word, get_wordnet_pos(word)) for word in review_temp)
+        review_temp = lemmatizer(review_temp)
+        review_temp = [token.lemma_ if token.lemma_ != '-PRON-' else '' for token in review_temp]
         review_temp = ' '.join([word for word in review_temp if word not in stopwords.words('english')])
         new_review_data = (review_temp, each[1])
         bulk_data.append(new_review_data)
     filling_the_database(bulk_data)
-    # print('##AFTER##',review_temp)
 
 
 if __name__ == "__main__":
+
     review_data = ()
     bulk_data = []
     amount_of_reviews = 0
@@ -62,12 +48,13 @@ if __name__ == "__main__":
     c.execute("""CREATE TABLE IF NOT EXISTS preprocessed_reviews(review TEXT, review_score INTEGER, ID PRIMARY KEY)""")
     conn.commit()
     total_number_of_reviews = c.execute("""SELECT COUNT(*) FROM movie_reviews""")
+
     for each in total_number_of_reviews:
         total_number_of_reviews = each[0]
-    counter = 0
-    while amount_of_reviews < 5000:
+
+    while amount_of_reviews <= total_number_of_reviews:
         review_package = []
-        amount_of_reviews += 50
+        amount_of_reviews += 100
         data = c.execute("""SELECT * FROM movie_reviews WHERE ID BETWEEN (?) AND (?)""", (previous_amount, amount_of_reviews-1))
         conn.commit()
         previous_amount = amount_of_reviews
@@ -79,7 +66,6 @@ if __name__ == "__main__":
         del review_package
         print(amount_of_reviews)
 
-    # review_cleaner(bulk_data)
     p = Pool(4)
     p.map(review_cleaner, bulk_data)
     p.close()
